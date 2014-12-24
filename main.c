bacc1f6cb21ae11a12c73f71b06b5106ad1906ca
Erik Nordstroem <contact@erikano.net>
2014-12-24 16:52:31 +0100

    Error message.

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <stdio.h>
+
 #include <sys/types.h>
 #include <db_185.h>
 #include <fcntl.h>
@@ -10,6 +12,7 @@ int main (int argc, char *argv[])
   DB *db = dbopen(f, O_CREAT | O_RDWR, 0, DB_HASH, NULL);
   if (db == NULL)
   {
+    fprintf(stderr, "dbopen failed.\n");
     exit(EXIT_FAILURE);
   }
 }
