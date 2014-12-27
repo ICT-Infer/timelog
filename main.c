04fc77a1527e42a36f3310f9cf7b45b46194d0d6
Erik Nordstroem <contact@erikano.net>
2014-12-28 00:03:41 +0100

    tl database file as argument instead of as environment variable.

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include <sys/types.h>
 #include <db_185.h>
@@ -9,9 +10,10 @@
 void usage(char* pname[])
 {
   fprintf(stderr, "Usage:\n");
-  fprintf(stderr, " %s begin <location> [-c <comment>] [<ts>]\n", *pname);
-  fprintf(stderr, " %s end <id> <location> [-c <comment>] [<ts>]\n", *pname);
-  fprintf(stderr, " %s report [<ts> [<ts>]]\n", *pname);
+  fprintf(stderr, " %s create [-f <file.db>]\n", *pname);
+  fprintf(stderr, " %s begin <location> [-c <comment>] [<ts>] [-f <file.db>]\n", *pname);
+  fprintf(stderr, " %s end <id> <location> [-c <comment>] [<ts>] [-f <file.db>]\n", *pname);
+  fprintf(stderr, " %s report [<ts> [<ts>]] [-f <file.db>]\n", *pname);
 }
 
 void tl_begin(int cmd_argc, char* cmd_argv[], DB* tl_db, char* pname[])
@@ -36,38 +38,61 @@ int main (int argc, char* argv[])
     exit(EXIT_FAILURE);
   }
 
-  char* f = getenv("TLDATABASE");
-  if (f == NULL)
+  char* f;
+  int cmd_argc = argc;
+  if (argc > 3 && strcmp(argv[argc - 2], "-f") == 0)
+  {
+    //strcpy(f, argv[argc - 1]);
+    f = argv[argc - 1];
+    cmd_argc -= 2;
+  } else
   {
     f = "tl_default.db";
-  }
-  fprintf(stderr, "Using tl database %s.\n", f);
-
-  DB* tl_db = dbopen(f, O_CREAT | O_RDWR, 0, DB_HASH, NULL);
-  if (tl_db == NULL)
-  {
-    fprintf(stderr, "dbopen failed.\n");
-    exit(EXIT_FAILURE);
+    fprintf(stderr, "Using tl database %s.\n", f);
   }
 
   char** cmd = &(argv[1]);
-  int cmd_argc = argc - 2;
+  cmd_argc -= 1;
   char** cmd_argv = &(argv[2]);
-  if (strcmp(*cmd, "begin") == 0)
+  if (strcmp(*cmd, "create") == 0)
   {
-    tl_begin(cmd_argc, cmd_argv, tl_db, pname);
-  } else if (strcmp(*cmd, "end") == 0)
-  {
-    tl_end(cmd_argc, cmd_argv, tl_db, pname);
-  } else if (strcmp(*cmd, "report") == 0)
-  {
-    tl_report(cmd_argc, cmd_argv, tl_db, pname);
-  } else
-  {
-    fprintf(stderr, "%s: Unknown command %s.\n\n", *pname, *cmd);
-    usage(pname);
-    exit(EXIT_FAILURE);
+    if (cmd_argc > 1)
+    {
+      fprintf(stderr, "%s: create: %d additional argument(s) passed. First: `%s'.\n", *pname, cmd_argc, *cmd_argv);
+      usage(pname);
+      exit(EXIT_FAILURE);
+    }
+    DB* tl_db = dbopen(f, O_CREAT | O_RDWR, 0, DB_HASH, NULL);
+    if (tl_db == NULL)
+    {
+      fprintf(stderr, "%s: Failed to create tl database `%s'.\n", *pname, *f);
+      exit(EXIT_FAILURE);
+    }
+  } else {
+    DB* tl_db = dbopen(f, O_RDWR, 0, DB_HASH, NULL);
+    if (tl_db == NULL)
+    {
+      fprintf(stderr, "%s: Failed to open tl database `%s'.\n", *pname, *f);
+      exit(EXIT_FAILURE);
+    }
+
+    if (strcmp(*cmd, "begin") == 0)
+    {
+      tl_begin(cmd_argc, cmd_argv, tl_db, pname);
+    } else if (strcmp(*cmd, "end") == 0)
+    {
+      tl_end(cmd_argc, cmd_argv, tl_db, pname);
+    } else if (strcmp(*cmd, "report") == 0)
+    {
+      tl_report(cmd_argc, cmd_argv, tl_db, pname);
+    } else
+    {
+      fprintf(stderr, "%s: Unknown command %s.\n\n", *pname, *cmd);
+      usage(pname);
+      exit(EXIT_FAILURE);
+    }
   }
+
 
   exit(EXIT_SUCCESS);
 }
--- test.sh
+++ test.sh
@@ -4,17 +4,43 @@ n_tests=0
 n_tests_failed=0
 n_tests_passed=0
 
-export TLDATABASE=tl_test.db
+TLDATABASE="tl_test.db"
 
-if [ -f $TLDATABASE ] ; then
+if [ -f "$TLDATABASE" ] ; then
   echo "tl db file $TLDATABASE exists. Cannot run tests." 1>&2
   exit 1
 fi
 
 n_tests=$(( $n_tests + 1 ))
+./bin/tl create -f "$TLDATABASE" 2>/dev/null
+if [ $? -ne 0 ] ; then
+  echo 'Test: Create named tl database. Failed.' 1>&2
+  n_tests_failed=$(( $n_tests_failed + 1 ))
+else
+  n_tests_passed=$(( $n_tests_passed + 1 ))
+fi
+
+n_tests=$(( $n_tests + 1 ))
+if [ ! -f "$TLDATABASE" ] ; then
+  echo 'Test: Created named tl database file. Failed.' 1>&2
+  n_tests_failed=$(( $n_tests_failed + 1 ))
+else
+  n_tests_passed=$(( $n_tests_passed + 1 ))
+fi
+
+n_tests=$(( $n_tests + 1 ))
 ./bin/tl 2>/dev/null
 if [ $? -eq 0 ] ; then
-  echo 'Argument count test failed.' 1>&2
+  echo 'Test: No command provided #1. Failed.' 1>&2
+  n_tests_failed=$(( $n_tests_failed + 1 ))
+else
+  n_tests_passed=$(( $n_tests_passed + 1 ))
+fi
+
+n_tests=$(( $n_tests + 1 ))
+./bin/tl -f "$TLDATABASE" 2>/dev/null
+if [ $? -eq 0 ] ; then
+  echo 'Test: No command provided #2. Failed.' 1>&2
   n_tests_failed=$(( $n_tests_failed + 1 ))
 else
   n_tests_passed=$(( $n_tests_passed + 1 ))
@@ -23,23 +49,28 @@ fi
 n_tests=$(( $n_tests + 1 ))
 ./bin/tl x 2>/dev/null
 if [ $? -eq 0 ] ; then
-  echo 'Command name test failed.' 1>&2
+  echo 'Test: Invalid command #1. Failed.' 1>&2
   n_tests_failed=$(( $n_tests_failed + 1 ))
 else
   n_tests_passed=$(( $n_tests_passed + 1 ))
 fi
 
 n_tests=$(( $n_tests + 1 ))
-if [ -f "$TLDATABASE" ] ; then
-  rm "$TLDATABASE"
-  if [ $? -ne 0 ] ; then
-    n_tests_failed=$(( $n_tests_failed + 1 ))
-  else
-    n_tests_passed=$(( $n_tests_passed + 1 ))
-  fi
-else
-  echo 'Test for existence of tl database failed.' 1>&2
+./bin/tl x -f "$TLDATABASE" 2>/dev/null
+if [ $? -eq 0 ] ; then
+  echo 'Test: Invalid command #2. Failed.' 1>&2
   n_tests_failed=$(( $n_tests_failed + 1 ))
+else
+  n_tests_passed=$(( $n_tests_passed + 1 ))
+fi
+
+n_tests=$(( $n_tests + 1 ))
+rm "$TLDATABASE" 2>/dev/null
+if [ $? -ne 0 ] ; then
+  echo 'Test: Remove tl database file. Failed.' 1>&2
+  n_tests_failed=$(( $n_tests_failed + 1 ))
+else
+  n_tests_passed=$(( $n_tests_passed + 1 ))
 fi
 
 echo "Ran $n_tests tests. $n_tests_passed passed. $n_tests_failed failed."
