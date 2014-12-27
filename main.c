29a0d5c8035676e56d3a0ac384036f576fd85c0b
Erik Nordstrøm <erik@nordstroem.no>
2014-12-27 20:51:07 +0100

    argc < 2? Usage.

--- main.c
+++ main.c
@@ -6,8 +6,25 @@
 #include <fcntl.h>
 #include <limits.h>
 
+void usage(char* pname[])
+{
+  fprintf(stderr, "Usage:\n");
+  fprintf(stderr, " %s begin <location> [-c <comment>] [<ts>]\n", *pname);
+  fprintf(stderr, " %s end <id> <location> [-c <comment>] [<ts>]\n", *pname);
+  fprintf(stderr, " %s report [<ts> [<ts>]]\n", *pname);
+}
+
 int main (int argc, char* argv[])
 {
+  char** pname = &(argv[0]);
+
+  if (argc < 2)
+  {
+    fprintf(stderr, "%s: No command provided.\n\n", *pname);
+    usage(pname);
+    exit(EXIT_FAILURE);
+  }
+
   char* f = "tl_default.db";
   DB* db = dbopen(f, O_CREAT | O_RDWR, 0, DB_HASH, NULL);
   if (db == NULL)
