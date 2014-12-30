983e75811b9a051d496eedf28a7df0ae83cb0b2b
Erik Nordstrøm <erik@nordstroem.no>
2014-12-30 11:46:37 +0100

    tl db file default mode 00644.

--- main.c
+++ main.c
@@ -32,7 +32,7 @@ DB* tl_dbcreate(char* f, const char* pname)
 {
   tl_dbfile(&f, pname);
 
-  DB* tl_db = dbopen(f, O_CREAT | O_EXCL | O_RDWR, 0, DB_HASH, NULL);
+  DB* tl_db = dbopen(f, O_CREAT | O_EXCL | O_RDWR, 00644, DB_HASH, NULL);
   if (tl_db == NULL)
   {
     fprintf(stderr, "%s: Failed to create tl database `%s'.\n", pname, f);
@@ -46,7 +46,7 @@ DB* tl_dbopen(char* f, const char* pname)
 {
   tl_dbfile(&f, pname);
 
-  DB* tl_db = dbopen(f, O_RDWR, 0, DB_HASH, NULL);
+  DB* tl_db = dbopen(f, O_RDWR, 00644, DB_HASH, NULL);
   if (tl_db == NULL)
   {
     fprintf(stderr, "%s: Failed to open tl database `%s'.\n", pname, f);
