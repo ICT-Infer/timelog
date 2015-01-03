90e228f4f64909c35f1c906c4a432ab276e4c122
Erik Nordstrøm <erik@nordstroem.no>
2015-01-03 19:55:08 +0100

    Rename commands, add more commands.

--- test.sh
+++ test.sh
@@ -12,7 +12,7 @@ if [ -f "$TLDATABASE" ] ; then
 fi
 
 n_tests=$(( $n_tests + 1 ))
-./bin/tl create -f "$TLDATABASE" 2>/dev/null
+./bin/tl create-db -f "$TLDATABASE" 2>/dev/null
 if [ $? -ne 0 ] ; then
   echo 'Test: Create named tl database. Failed.' 1>&2
   n_tests_failed=$(( $n_tests_failed + 1 ))
@@ -21,7 +21,7 @@ else
 fi
 
 n_tests=$(( $n_tests + 1 ))
-./bin/tl create -f "$TLDATABASE" 2>/dev/null
+./bin/tl create-db -f "$TLDATABASE" 2>/dev/null
 if [ $? -eq 0 ] ; then
   echo 'Test: Create existing named tl database. Failed.' 1>&2
   n_tests_failed=$(( $n_tests_failed + 1 ))
