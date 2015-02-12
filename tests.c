995988017be74dec6495d2ecd49ba904957f2d23
Erik Nordstroem <contact@erikano.net>
2015-02-12 19:21:41 +0100

    In tests.c, fix warnings about mixing declarations and code.

--- tests.c
+++ tests.c
@@ -38,32 +38,15 @@ int main()
   int errors = 0;    /* Errors in the test program itself. */
   int nt_passed = 0; /* Number of tests passed. */
   int nt_failed = 0; /* Number of tests failed. */
-
   char odir[MAXPATHLEN];
-  if (getcwd(odir, sizeof(odir)) == NULL)
-  {
-    fprintf(stderr, "`getcwd' failed.\n");
-    exit(EXIT_FAILURE);
-  }
-
   char tlb[MAXPATHLEN];
   struct stat stlb;
-  if (strlcpy(tlb, odir, sizeof(tlb)) > sizeof(tlb) ||
-      strlcat(tlb, "/bin/tl", sizeof(tlb)) > sizeof(tlb) ||
-      stat(tlb, &stlb) != 0)
-  {
-    fprintf(stderr, "Creation of path to `tl' binary failed.\n");
-    exit(EXIT_FAILURE);
-  }
-
-  /* Temporary directory in which tests will be run. */
   char template[] = "/tmp/tl-XXXXXXXX";
   char *tmpdir = mkdtemp(template);
-  if (chdir(tmpdir) != 0)
-  {
-    fprintf(stderr, "`chdir' into temporary directory `%s' failed.\n", tmpdir);
-    exit(EXIT_FAILURE);
-  }
+  const char f_tldir[] = ".tl/";
+  const char f_tldb[] = "tl.db";
+  const char f_tps[] = "tps.db";
+  int i;
 
   tltest tests[] = {
       {true, "`tl init'", (char *[]){tlb, "init", NULL}},
@@ -153,7 +136,27 @@ int main()
 
       {true, "`tl report' with empty log", (char *[]){tlb, "report", NULL}}};
 
-  int i;
+  if (getcwd(odir, sizeof(odir)) == NULL)
+  {
+    fprintf(stderr, "`getcwd' failed.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if (strlcpy(tlb, odir, sizeof(tlb)) > sizeof(tlb) ||
+      strlcat(tlb, "/bin/tl", sizeof(tlb)) > sizeof(tlb) ||
+      stat(tlb, &stlb) != 0)
+  {
+    fprintf(stderr, "Creation of path to `tl' binary failed.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  /* Temporary directory in which tests will be run. */
+  if (chdir(tmpdir) != 0)
+  {
+    fprintf(stderr, "`chdir' into temporary directory `%s' failed.\n", tmpdir);
+    exit(EXIT_FAILURE);
+  }
+
   for (i = 0; i < sizeof(tests) / sizeof(tests[0]); i++)
   {
     pid_t pid = fork();
@@ -185,9 +188,6 @@ int main()
     }
   }
 
-  const char f_tldir[] = ".tl/";
-  const char f_tldb[] = "tl.db";
-  const char f_tps[] = "tps.db";
   if (chdir(f_tldir) == 0)
   {
     unlink(f_tldb);
