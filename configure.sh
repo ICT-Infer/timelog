fbcee7dd6c72f045eb7cc497a2bee71be3c5063b
Erik Nordstrøm <erik@nordstroem.no>
2015-02-27 22:12:30 +0100

    Phony target to make pretty.

--- configure.sh
+++ configure.sh
@@ -68,4 +68,21 @@ tests: tests.c
 clean:
 	rm -rf bin/ lib/ oobj/
 	rm -f tests
+.PHONY: pretty
+pretty:
+	echo | clang-format >/dev/null # Instead of \`which' because on
+	                               # Mac OS X 10.4.11, \`which'
+	                               # exits with 0 regardless of whether
+	                               # whether or not the program was found.
+	clang-format timelog.c > timelog.c_
+	diff timelog.c timelog.c_ >/dev/null && \\
+		mv timelog.c_ timelog.c || rm timelog.c_
+	clang-format tl.c > tl.c_
+	diff tl.c tl.c_ >/dev/null && mv tl.c_ tl.c || rm tl.c_
+	clang-format include/timelog.h > include/timelog.h_
+	diff include/timelog.h include/timelog.h_ >/dev/null && \\
+		mv include/timelog.h_ include/timelog.h \\
+		|| rm include/timelog.h_
+	clang-format tests.c > tests.c_
+	diff tests.c tests.c_ >/dev/null && mv tests.c_ tests.c || rm tests.c_
 EOF
