85300a670e5973c362ccfeb7934a5573d2efe54d
Erik Nordstrøm <erik@nordstroem.no>
2015-02-27 23:29:37 +0100

    Ugly. Especially the malloc stuff which shouldn't be necessary and which I don't even free. Sorry :/

--- tests.c
+++ tests.c
@@ -15,14 +15,40 @@
  */
 
 #define NUMTESTS 28
-#define TESTNA(xsv, dv) tests[++i_tests]; \
+
+#define TEST0(xsv, dv) tests[++i_tests]; \
 tests[i_tests].xsuccess = xsv; \
 tests[i_tests].desc = dv; \
-tests[i_tests].argv = (char *[]){tlb, NULL};
-#define TEST(xsv, dv, ...) tests[++i_tests]; \
+tests[i_tests].argv = malloc(4096); \
+tests[i_tests].argv[0] = tlb; \
+tests[i_tests].argv[1] = NULL;
+
+#define TEST1(xsv, dv, argv1) tests[++i_tests]; \
 tests[i_tests].xsuccess = xsv; \
 tests[i_tests].desc = dv; \
-tests[i_tests].argv = (char *[]){tlb, __VA_ARGS__, NULL};
+tests[i_tests].argv = malloc(4096); \
+tests[i_tests].argv[0] = tlb; \
+tests[i_tests].argv[1] = argv1; \
+tests[i_tests].argv[2] = NULL;
+
+#define TEST2(xsv, dv, argv1, argv2) tests[++i_tests]; \
+tests[i_tests].xsuccess = xsv; \
+tests[i_tests].desc = dv; \
+tests[i_tests].argv = malloc(4096); \
+tests[i_tests].argv[0] = tlb; \
+tests[i_tests].argv[1] = argv1; \
+tests[i_tests].argv[2] = argv2; \
+tests[i_tests].argv[3] = NULL;
+
+#define TEST3(xsv, dv, argv1, argv2, argv3) tests[++i_tests]; \
+tests[i_tests].xsuccess = xsv; \
+tests[i_tests].desc = dv; \
+tests[i_tests].argv = malloc(4096); \
+tests[i_tests].argv[0] = tlb; \
+tests[i_tests].argv[1] = argv1; \
+tests[i_tests].argv[2] = argv2; \
+tests[i_tests].argv[3] = argv3; \
+tests[i_tests].argv[4] = NULL;
 
 #include <sys/param.h>
 #include <stdlib.h>
@@ -61,48 +87,48 @@ int main()
   tltest tests[NUMTESTS];
   int i_tests = -1;
 
-  TEST(true, "`tl init'", "init");
-  TEST(false, "`tl init' in directory with existing timelog", "init");
-  TESTNA(false, "`tl' without arguments");
-  TEST(false, "`tl x' (invalid command)", "x");
-  TEST(true, "`tl pending' with empty stack", "pending");
-  TEST(false, "`tl pop-drop' with empty stack", "pop-drop");
-  TEST(false, "`tl merge-add' with empty stack", "merge-add");
-  TEST(true, "`tl timepoint'", "timepoint");
-  TEST(true, "`tl pending' with one element on stack", "pending");
-  TEST(true, "`tl pop-drop' with one element on stack", "pop-drop");
-  TEST(true, "`tl timepoint -l <loc>' with valid length loc", "timepoint",
+  TEST1(true, "`tl init'", "init");
+  TEST1(false, "`tl init' in directory with existing timelog", "init");
+  TEST0(false, "`tl' without arguments");
+  TEST1(false, "`tl x' (invalid command)", "x");
+  TEST1(true, "`tl pending' with empty stack", "pending");
+  TEST1(false, "`tl pop-drop' with empty stack", "pop-drop");
+  TEST1(false, "`tl merge-add' with empty stack", "merge-add");
+  TEST1(true, "`tl timepoint'", "timepoint");
+  TEST1(true, "`tl pending' with one element on stack", "pending");
+  TEST1(true, "`tl pop-drop' with one element on stack", "pop-drop");
+  TEST3(true, "`tl timepoint -l <loc>' with valid length loc", "timepoint",
     "-l", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxy");
-  TEST(false, "`tl timepoint -l <loc>' with invalid length loc", "timepoint",
+  TEST3(false, "`tl timepoint -l <loc>' with invalid length loc", "timepoint",
     "-l", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxyz");
-  TEST(true, "`tl timepoint -m <msg>' with valid length msg", "timepoint",
+  TEST3(true, "`tl timepoint -m <msg>' with valid length msg", "timepoint",
     "-m", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxy");
-  TEST(false, "`tl timepoint -m <msg>' with invalid length msg", "timepoint",
+  TEST3(false, "`tl timepoint -m <msg>' with invalid length msg", "timepoint",
     "-m", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxyz");
-  TEST(true, "`tl timepoint -t <ts>' with valid ts #1", "timepoint", "-t",
+  TEST3(true, "`tl timepoint -t <ts>' with valid ts #1", "timepoint", "-t",
     "22:22");
   /* In timezone Europe/Oslo, calendar time will contain 0x0a. */
-  TEST(true, "`tl timepoint -t <ts>' with valid ts #2", "timepoint", "-t",
+  TEST3(true, "`tl timepoint -t <ts>' with valid ts #2", "timepoint", "-t",
     "2015-01-30T23:50");
-  TEST(true, "`tl timepoint -t <ts>' with valid ts #3", "timepoint", "-t",
-    "2015-01-31T01:02", NULL);
-  TEST(true, "`tl pop-drop' to see if tps db was corrupted by valid ts #2",
+  TEST3(true, "`tl timepoint -t <ts>' with valid ts #3", "timepoint", "-t",
+    "2015-01-31T01:02");
+  TEST1(true, "`tl pop-drop' to see if tps db was corrupted by valid ts #2",
     "pop-drop");
-  TEST(false, "`tl timepoint -t <ts>' with invalid ts #1", "timepoint", "-t",
+  TEST3(false, "`tl timepoint -t <ts>' with invalid ts #1", "timepoint", "-t",
     "T22:22");
-  TEST(false, "`tl timepoint -t <ts>' with invalid ts #2", "timepoint", "-t",
+  TEST3(false, "`tl timepoint -t <ts>' with invalid ts #2", "timepoint", "-t",
     "22:22:00");
-  TEST(false, "`tl timepoint -t <ts>' with invalid ts #3", "timepoint", "-t",
+  TEST3(false, "`tl timepoint -t <ts>' with invalid ts #3", "timepoint", "-t",
     "2015-01-10t22:22");
-  TEST(false, "`tl timepoint -t <ts>' with invalid ts #4", "timepoint", "-t",
+  TEST3(false, "`tl timepoint -t <ts>' with invalid ts #4", "timepoint", "-t",
     "2014-02-31T22:22");
-  TEST(true, "`tl merge-add' with multiple points on stack", "merge-add");
-  TEST(true, "`tl report' with one entry in log", "report");
-  TEST(false, "`tl unlog' 2nd log entry with only one entry in log", "unlog",
+  TEST1(true, "`tl merge-add' with multiple points on stack", "merge-add");
+  TEST1(true, "`tl report' with one entry in log", "report");
+  TEST2(false, "`tl unlog' 2nd log entry with only one entry in log", "unlog",
     "2");
-  TEST(true, "`tl unlog' 1st log entry", "unlog", "1");
-  TEST(false, "`tl unlog' 1st log entry again", "unlog", "1");
-  TEST(true, "`tl report' with empty log", "report");
+  TEST2(true, "`tl unlog' 1st log entry", "unlog", "1");
+  TEST2(false, "`tl unlog' 1st log entry again", "unlog", "1");
+  TEST1(true, "`tl report' with empty log", "report");
 
   if (getcwd(odir, sizeof(odir)) == NULL)
   {
