f974bc9e91c36a07862c4076f8b9a4e98716c8b7
Erik Nordstrøm <erik@nordstroem.no>
2015-03-11 22:40:08 +0100

    Use --exclude-standard for git ls-files when determining untracked changes.

--- version.sh
+++ version.sh
@@ -17,7 +17,7 @@ git diff-files --quiet
 if [ $? -ne 0 ] ; then repo_unstaged=f ; fi
 git diff-index --quiet HEAD
 if [ $? -ne 0 ] ; then repo_stagecombo=i ; fi
-test -z "$( git ls-files --others )"
+test -z "$( git ls-files --others --exclude-standard )"
 if [ $? -ne 0 ] ; then repo_untracked=o ; fi
 repo_status="${repo_staged}${repo_unstaged}${repo_stagecombo}${repo_untracked}"
 test -z "${repo_status}"
