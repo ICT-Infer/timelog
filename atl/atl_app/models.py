3ecc656b6a71744394ba83a6f463883ec90ee3d6
Erik Nordstrøm <erik@nordstroem.no>
2015-03-17 14:45:19 +0000

    Removed intital SQL schema, created django app model.

--- atl/atl_app/models.py
+++ atl/atl_app/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+  parent = models.ForeignKey("self")
+  name = models.CharField(max_length=255)
+  description = models.CharField(max_length=255)
+
+class Entry(models.Model):
+  user = models.ForeignKey(settings.AUTH_USER_MODEL)
+  project = models.ForeignKey(Project)
+  t_begin = models.DateTimeField('t_begin')
+  t_end = models.DateTimeField('t_end')
+  description = models.CharField(max_length=255)
