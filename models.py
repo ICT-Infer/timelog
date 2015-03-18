4b93ecbebf3cf98554505c64fcf899f4e92f862f
Erik Nordstroem <contact@erikano.net>
2015-03-18 14:18:14 +0000

    String representations of models.

--- models.py
+++ models.py
@@ -6,12 +6,19 @@ class Project(models.Model):
   name = models.CharField(max_length=255)
   description = models.CharField(max_length=255, blank=True)
 
+  def __str__(self):
+    return self.name
+
 class Entry(models.Model):
   user = models.ForeignKey(settings.AUTH_USER_MODEL)
   project = models.ForeignKey(Project, null=True)
   t_begin = models.DateTimeField('t_begin', null=True)
   t_end = models.DateTimeField('t_end', null=True)
   description = models.CharField(max_length=255, blank=True)
+
+  def __str__(self):
+    return str(self.user) + ', ' + str(self.project) + ' @ ' \
+      + str(self.t_begin) + ' - ' + str(self.t_end)
 
   class Meta:
     verbose_name_plural = 'entries'
