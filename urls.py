79ce8db786863628c2b304df7d6795cf57dd3b37
Erik Nordstroem <contact@erikano.net>
2015-03-24 18:46:43 +0000

    So I heard you like JSON.

--- urls.py
+++ urls.py
@@ -3,5 +3,5 @@ from timelog import views
 
 urlpatterns = patterns('',
   url(r'^$', views.index, name='index'),
-  url(r'^hours/sheets/sheet-(?P<arg_cat_id>\d+)-(?P<arg_year>\d+)-(?P<arg_month>\d{2})\.htm$', views.sheets, name='sheets'),
+  url(r'^hours/sheets/sheet-(?P<arg_cat_id>\d+)-(?P<arg_year>\d+)-(?P<arg_month>\d{2})\.(?P<arg_fmt_ext>\w+)$', views.sheets, name='sheets'),
 )
