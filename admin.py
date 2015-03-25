4eac44183963871fc228c0075f8fd501b22b6488
Erik Nordstrøm <erik@nordstroem.no>
2015-03-25 02:48:57 +0000

    Heh.

--- admin.py
+++ admin.py
@@ -6,14 +6,14 @@ class EntryAdmin (admin.ModelAdmin):
   def get_form (self, req, obj=None, **kwargs):
     if obj is not None:
       # Reverse of what we do in models.Entry.save.
-      timezone.activate(obj.tz_begin)
-      obj.t_begin = timezone.make_naive(obj.t_begin, timezone.get_current_timezone())
       timezone.deactivate()
+      obj.t_begin = timezone.make_naive(obj.t_begin, timezone.get_current_timezone())
+      timezone.activate(obj.tz_begin)
       obj.t_begin = timezone.make_aware(obj.t_begin, timezone.get_current_timezone())
 
-      timezone.activate(obj.tz_end)
-      obj.t_end = timezone.make_naive(obj.t_end, timezone.get_current_timezone())
       timezone.deactivate()
+      obj.t_end = timezone.make_naive(obj.t_end, timezone.get_current_timezone())
+      timezone.activate(obj.tz_end)
       obj.t_end = timezone.make_aware(obj.t_end, timezone.get_current_timezone())
 
     return super(EntryAdmin, self).get_form(req, obj, **kwargs)
