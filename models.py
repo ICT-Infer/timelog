a12369b765e20614bf0456d4b5f1fadda7db4c38
Erik Nordstroem <contact@erikano.net>
2015-03-25 15:00:36 +0000

    Initial work on Entry data validation. The utcoffset checks are because of the way we replace time zone with user provided value elsewhere. The check to see if t_end is less than t_begin resolves issue #9.

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.core.exceptions import ValidationError
 import pytz
 
 class Category(models.Model):
@@ -23,6 +24,21 @@ class Entry(models.Model):
   t_end = models.DateTimeField('t_end', null=True, blank=True)
   tz_end = models.CharField(max_length=255, default=str(timezone.get_current_timezone()))
   description = models.CharField(max_length=255, blank=True)
+
+  def clean (self):
+    # Validation of the model as a whole
+
+    if (self.t_begin.tzinfo.utcoffset(None) != timezone.get_current_timezone().utcoffset(None)):
+      raise ValidationError("t_begin.tzinfo.utcoffset(None) must be equal to " \
+                            + "timezone.get_current_timezone().utcoffset(None).")
+    if (self.t_end and self.t_end.tzinfo.utcoffset(None) != timezone.get_current_timezone().utcoffset(None)):
+      raise ValidationError("t_end.tzinfo.utcoffset(None) must be equal to " \
+                            + "timezone.get_current_timezone().utcoffset(None).")
+
+    if (self.t_end and self.t_end < self.t_begin):
+      raise ValidationError("t_end shall not be < t_begin when t_end is not None")
+
+    super(Entry, self).clean()
 
   def save (self, *args, **kwargs):
     self.t_begin = timezone.make_naive(self.t_begin, timezone.get_current_timezone())
