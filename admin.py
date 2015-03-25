4ab58a8becbc57ec3781e40a95151bddb49fd263
Erik Nordstroem <contact@erikano.net>
2015-03-25 02:42:37 +0000

    A bit ugly. Define and use admin.EntryAdmin. It has a method get_form which performs the reverse operations on object t_begin and t_end do. This way, when editing an entry, the time and date will end up with the values they should have.

--- admin.py
+++ admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
+from django.utils import timezone
 from timelog.models import Category, Entry
 
+class EntryAdmin (admin.ModelAdmin):
+  def get_form (self, req, obj=None, **kwargs):
+    if obj is not None:
+      # Reverse of what we do in models.Entry.save.
+      timezone.activate(obj.tz_begin)
+      obj.t_begin = timezone.make_naive(obj.t_begin, timezone.get_current_timezone())
+      timezone.deactivate()
+      obj.t_begin = timezone.make_aware(obj.t_begin, timezone.get_current_timezone())
+
+      timezone.activate(obj.tz_end)
+      obj.t_end = timezone.make_naive(obj.t_end, timezone.get_current_timezone())
+      timezone.deactivate()
+      obj.t_end = timezone.make_aware(obj.t_end, timezone.get_current_timezone())
+
+    return super(EntryAdmin, self).get_form(req, obj, **kwargs)
+
 admin.site.register(Category)
-admin.site.register(Entry)
+admin.site.register(Entry, EntryAdmin)
