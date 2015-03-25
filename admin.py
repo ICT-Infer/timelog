9302558a410aa8a52bb28580ee251984220525a4
Erik Nordstroem <contact@erikano.net>
2015-03-25 04:04:36 +0000

    Derp indeed. I am doing something horribly wrong and it's still not right and I am way too tired to understand what I ammdoing now.

--- admin.py
+++ admin.py
@@ -1,20 +1,18 @@
 from django.contrib import admin
 from django.utils import timezone
 from timelog.models import Category, Entry
+import pytz
 
 class EntryAdmin (admin.ModelAdmin):
   def get_form (self, req, obj=None, **kwargs):
     if obj is not None:
       # Reverse of what we do in models.Entry.save.
-      obj.t_begin = timezone.make_naive(obj.t_begin, timezone.get_current_timezone())
-      timezone.activate(obj.tz_begin)
+      obj.t_begin = obj.t_begin.astimezone(pytz.timezone(obj.tz_begin)).replace(tzinfo=None)
       obj.t_begin = timezone.make_aware(obj.t_begin, timezone.get_current_timezone())
-      timezone.deactivate()
 
-      obj.t_end = timezone.make_naive(obj.t_end, timezone.get_current_timezone())
-      timezone.activate(obj.tz_end)
-      obj.t_end = timezone.make_aware(obj.t_end, timezone.get_current_timezone())
-      timezone.deactivate()
+      if obj.t_end:
+        obj.t_end = obj.t_end.astimezone(pytz.timezone(obj.tz_end)).replace(tzinfo=None)
+        obj.t_end = timezone.make_aware(obj.t_end, timezone.get_current_timezone())
 
     return super(EntryAdmin, self).get_form(req, obj, **kwargs)
 
