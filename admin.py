808c3f98358945645fe0af8437065b386397d2ce
Erik Nordstrøm <erik@nordstroem.no>
2015-04-03 09:02:30 +0000

    find . -maxdepth 1 -name \*.py | xargs -I {} sh -c "python3 -m yapf --style='{based_on_style: pep8, indent_width: 2, continuation_indent_width: 2}' {} > {}.fmt ; mv {}.fmt {}"

--- admin.py
+++ admin.py
@@ -3,21 +3,26 @@ from django.utils import timezone
 from timelog.models import Category, Entry
 import pytz
 
-class EntryAdmin (admin.ModelAdmin):
 
-  def get_form (self, req, obj, **kwargs):
+class EntryAdmin(admin.ModelAdmin):
+  def get_form(self, req, obj, **kwargs):
     if obj is not None and not hasattr(obj, 'tz_maniped'):
       # Reverse of what we do in models.Entry.save.
-      obj.t_begin = obj.t_begin.astimezone(pytz.timezone(obj.tz_begin)).replace(tzinfo=None)
-      obj.t_begin = timezone.make_aware(obj.t_begin, timezone.get_current_timezone())
+      obj.t_begin = obj.t_begin.astimezone(
+        pytz.timezone(obj.tz_begin)).replace(tzinfo=None)
+      obj.t_begin = timezone.make_aware(obj.t_begin,
+                                        timezone.get_current_timezone())
 
       if obj.t_end:
-        obj.t_end = obj.t_end.astimezone(pytz.timezone(obj.tz_end)).replace(tzinfo=None)
-        obj.t_end = timezone.make_aware(obj.t_end, timezone.get_current_timezone())
+        obj.t_end = obj.t_end.astimezone(pytz.timezone(obj.tz_end)).replace(
+          tzinfo=None)
+        obj.t_end = timezone.make_aware(obj.t_end,
+                                        timezone.get_current_timezone())
 
       obj.tz_maniped = True
 
     return super(EntryAdmin, self).get_form(req, obj, **kwargs)
+
 
 admin.site.register(Category)
 admin.site.register(Entry, EntryAdmin)
--- urls.py
+++ urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import patterns, url
 from timelog import views
 
-urlpatterns = patterns('',
-  url(r'^$', views.index, name='index'),
-  url(r'^hours/sheets/index-(?P<arg_year>\d+)-(?P<arg_month>\d{2})\.(?P<arg_fmt_ext>\w+)$', views.sheets, name='sheets'),
-  url(r'^hours/sheets/sheet-(?P<arg_cat_slug>[0-9a-z_]+)-(?P<arg_year>\d+)-(?P<arg_month>\d{2})\.(?P<arg_fmt_ext>\w+)$', views.sheet, name='sheet'),
-)
+urlpatterns = patterns(
+  '', url(r'^$', views.index,
+          name='index'),
+  url(
+    r'^hours/sheets/index-(?P<arg_year>\d+)-(?P<arg_month>\d{2})\.(?P<arg_fmt_ext>\w+)$',
+    views.sheets,
+    name='sheets'),
+  url(
+    r'^hours/sheets/sheet-(?P<arg_cat_slug>[0-9a-z_]+)-(?P<arg_year>\d+)-(?P<arg_month>\d{2})\.(?P<arg_fmt_ext>\w+)$',
+    views.sheet,
+    name='sheet'), )
