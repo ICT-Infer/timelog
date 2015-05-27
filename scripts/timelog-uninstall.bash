811eaed5141c3ca3781c0969ab793e51c849a296
Erik Nordstrøm <erik@nordstroem.no>
2015-05-27 20:44:41 +0200

    Wrap timelog uninstallation script main contents in a function definition for consistency with the installation script.

--- scripts/timelog-uninstall.bash
+++ scripts/timelog-uninstall.bash
@@ -20,49 +20,58 @@ if [ ! "$( cat /etc/issue | head -n1 | cut -d' ' -f1-3 )" == "Debian GNU/Linux 8
   exit 3
 fi
 
-read -p "Stop timelog service and kill all timelog user owned processes? [y/N] " -n 1 -r
-if [ ! $REPLY == "" ] ; then
-  echo
-fi
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-  echo "Abort." 1>&2
-  exit 1
-fi
+function uninstall_timelog {
 
-systemctl stop timelog.service
-pkill -u timelog
-
-read -p "Really remove timelog service, user and files? [y/N] " -n 1 -r
-if [ ! $REPLY == "" ] ; then
-  echo
-fi
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-  echo "Abort." 1>&2
-  exit 1
-fi
-
-systemctl disable timelog.service
-rm /etc/systemd/system/timelog.service
-systemctl daemon-reload
-rm /etc/nginx/sites-available/timelog
-userdel timelog
-rm -rf /var/lib/timelog
-
-if [ "$drop" == "true" ] ; then
-  read -p "Really drop all timelog data from DB? [y/N] " -n 1 -r
+  read -p "Stop timelog service and kill all timelog user owned processes? [y/N] " -n 1 -r
   if [ ! $REPLY == "" ] ; then
     echo
   fi
-  if [[ $REPLY =~ ^[Yy]$ ]]
+  if [[ ! $REPLY =~ ^[Yy]$ ]]
   then
-    sudo -u postgres -i psql <<EOF
-DROP DATABASE timelog;
-DROP USER timelog;
-EOF
-  else
     echo "Abort." 1>&2
     exit 1
   fi
-fi
+
+  systemctl stop timelog.service
+  pkill -u timelog
+
+  read -p "Really remove timelog service, user and files? [y/N] " -n 1 -r
+  if [ ! $REPLY == "" ] ; then
+    echo
+  fi
+  if [[ ! $REPLY =~ ^[Yy]$ ]]
+  then
+    echo "Abort." 1>&2
+    exit 1
+  fi
+
+  systemctl disable timelog.service
+  rm /etc/systemd/system/timelog.service
+  systemctl daemon-reload
+  rm /etc/nginx/sites-available/timelog
+  userdel timelog
+  rm -rf /var/lib/timelog
+
+  if [ "$drop" == "true" ] ; then
+    read -p "Really drop all timelog data from DB? [y/N] " -n 1 -r
+    if [ ! $REPLY == "" ] ; then
+      echo
+    fi
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      sudo -u postgres -i psql <<EOF
+DROP DATABASE timelog;
+DROP USER timelog;
+EOF
+    else
+      echo "Abort." 1>&2
+      exit 1
+    fi
+  fi
+
+  echo "Done uninstalling timelog." 1>&2
+  echo "Dependencies installed using apt-get" \
+    "will have to be removed manually." 1>&2
+}
+
+timelog_uninstall
