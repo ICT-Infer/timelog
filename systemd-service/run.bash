812e4536d6cd5739e4bd0b873ac9300c42f54601
Erik Nordstroem <contact@erikano.net>
2015-05-27 15:44:12 +0000

    Rename directory where uwsgi config is located from "uwsgi" to "uwsgi-config".

--- systemd-service/run.bash
+++ systemd-service/run.bash
@@ -2,4 +2,4 @@
 
 export HOME=/var/lib/timelog
 source ~/venv/bin/activate
-uwsgi /var/lib/timelog/venv/serve/timelog/uwsgi/timelog.ini
+uwsgi /var/lib/timelog/venv/serve/timelog/uwsgi-config/timelog.ini
