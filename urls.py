8c3786c0c6c9cd3b6087f4bda34744c7ad3dc546
Erik Nordstrøm <erik@nordstroem.no>
2015-06-04 19:35:11 +0000

    Login required.

--- urls.py
+++ urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth import views as auth_views
 from timelog import views
 
 urlpatterns = patterns(
   '',
   url(r'^$', views.index, name='index'),
+  url(r'^login/$', auth_views.login, {'template_name': 'login.htm'}),
   url(r'^hours/sheets/index-(?P<arg_year>\d+)-(?P<arg_month>\d{2})\.(?P<arg_fmt_ext>\w+)$',
     views.sheets,
     name='sheets'),
