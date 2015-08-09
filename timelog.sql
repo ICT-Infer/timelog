a7b8e22a4ab845430db7a49547149044f211bb8f
Erik Nordstroem <erikn@LoBSD.org>
2015-08-10 01:55:24 +0200

    Initial new database schema.

---
+++ timelog.sql
@@ -0,0 +1,41 @@
+--
+-- Copyright (c) 2015 Erik Nordstroem <erikn@LoBSD.org>
+--
+-- Permission to use, copy, modify, and distribute this software for any
+-- purpose with or without fee is hereby granted, provided that the above
+-- copyright notice and this permission notice appear in all copies.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+-- WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+-- MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+-- ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+-- WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+-- ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+-- OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+--
+
+BEGIN;
+CREATE TABLE categories (
+  parent_id integer,
+
+  id      serial PRIMARY KEY,
+  name    varchar(255) NOT NULL,
+  comment varchar(255),
+  slug    char(32) NOT NULL,
+
+  FOREIGN KEY (parent_id) REFERENCES categories  
+);
+CREATE TABLE entries (
+  uid   integer NOT NULL,
+  catid integer NOT NULL,
+
+  id       serial PRIMARY KEY,
+  t_begin  timestamp with time zone NOT NULL,
+  tz_begin varchar(255) NOT NULL,
+  t_end    timestamp with time zone,
+  tz_end   varchar(255),
+  comment  varchar(255),
+
+  FOREIGN KEY (catid) REFERENCES categories 
+);
+COMMIT;
