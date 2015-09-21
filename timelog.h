387602a27932d80fc7e265208fd4018728fe409e
Erik Nordstrøm <erik@nordstroem.no>
2015-09-22 00:46:33 +0200

    Update e-mail address in remaining places.

--- timelog.h
+++ timelog.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Erik Nordstroem <erikn@SaaS-by-Erik.net>
+ * Copyright (c) 2015 Erik Nordstroem <erikn@ict-infer.no>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
