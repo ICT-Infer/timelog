10fa1d82b8227977f60d73f8ce0b5affe17c5ca5
Erik Nordstrøm <erik@nordstroem.no>
2017-09-26 22:42:31 +0200

    Move getWeek function to bottom of file.

--- client/html/timelog.js
+++ client/html/timelog.js
@@ -14,13 +14,6 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-// https://stackoverflow.com/questions/7765767/show-week-number-with-javascript
-Date.prototype.getWeek = function ()
-{
-    var onejan = new Date(this.getFullYear(),0,1);
-    return Math.ceil((((this - onejan) / 86400000) + onejan.getDay()+1)/7);
-};
-
 const now = new Date();
 const isoweek = now.getFullYear() + 'W' + now.getWeek();
 
@@ -260,3 +253,10 @@ function fullDrawContent ()
 }
 
 fullDrawContent();
+
+// https://stackoverflow.com/questions/7765767/show-week-number-with-javascript
+Date.prototype.getWeek = function ()
+{
+    var onejan = new Date(this.getFullYear(),0,1);
+    return Math.ceil((((this - onejan) / 86400000) + onejan.getDay()+1)/7);
+};
