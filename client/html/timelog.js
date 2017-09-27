76ec1ef06ea51ad2926767aaa16536df5feb076d
Erik Nordstrøm <erik@nordstroem.no>
2017-09-27 12:28:12 +0200

    Scroll to 8 AM on load.

--- client/html/timelog.js
+++ client/html/timelog.js
@@ -206,6 +206,8 @@ function sizeCanvases ()
 
 sizeCanvases();
 
+window.scroll(0, 8 * 4 * (quarter_hour_margin_top + quarter_hour_height + quarter_hour_margin_bottom));
+
 function fullDrawIsoweek ()
 {
 	ctx_isoweek.fillStyle = colors.white;
